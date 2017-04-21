83211dc5bab3b319ad3bd3a613e62ed6c817564c
yangpiao <main@qq.com>
2017-04-21 21:21:35 +0800

    yangpiaohome

--- components/home/home.js
+++ components/home/home.js
@@ -8,5 +8,30 @@ angular.module('homeModule',[])
                 css:'components/home/home.css'
             })
     })
-    .controller('homeCtrl',['$scope',function($scope){
+    .controller('homeCtrl',['$scope','$http',function($scope,$http){
+    	$http.get('json/home/1.json').success(function(res){
+    		
+    		$scope.homeBanner= res.data['5956'].list[0];
+    		$scope.boxSlide = res.data['13730'].list;
+    		
+    	});
+    	$http.get('json/home/niuzaiku.json').success(function(res){
+    		
+    		$scope.niuzaiku = res.data;
+    	});
+    	$http.get('json/home/2.json').success(function(res){
+    		console.log(res);
+    		$scope.morenProducts = res.data.list;
+    		
+    	});
+    	$http.get('json/home/3.json').success(function(res){
+    		console.log(res);
+    		$scope.morenProducts2 = res.data.list;
+    		
+    	});
+    	$http.get('json/home/4.json').success(function(res){
+    		console.log(res);
+    		$scope.morenProducts3 = res.data.list;
+    		
+    	})
     }]);
