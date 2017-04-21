4437469f5fd907d3e1f8a02ba47672a7df967e24
lxx <891057982@qq.com>
2017-04-21 20:42:04 +0800

    'lxx首页'

--- components/type/type.js
+++ components/type/type.js
@@ -11,14 +11,17 @@ angular.module('typeModule',[])
     .controller('typeCtrl',['$scope','$http',function($scope,$http){
         $http.get('json/classify/1.json').success(function (res) {
             var resdata=res.data;
-            console.log(res.data)
             for (var i in resdata){
                 if (resdata[i].list.length>10){
                     $scope.type_arr1=resdata[i].list;
                 }
-
-
-
             }
+        })
+        $http.get('json/classify/2.json').success(function (res1) {
+            $scope.type_arr2=res1.data;
+
+        })
+        $http.get('json/classify/3.json').success(function (res2) {
+
         })
     }]);
