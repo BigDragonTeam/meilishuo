ea48b3eae45dd45e078785377ee71cbadd4643f3
thl <852537973@qq.com>
2017-04-21 11:35:49 +0800

    'thl,rem'

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ angular.module('mls',["ui.router",'angularCSS','homeModule','typeModule','cartMo
     .config(function($stateProvider, $urlRouterProvider){
         $urlRouterProvider.otherwise('/home');
     });
+document.documentElement.style.fontSize = document.documentElement.clientWidth/7.5+ 'px';
+window.addEventListener('resize',function(){
+    document.documentElement.style.fontSize = innerWidth/7.5+'px';
+});
