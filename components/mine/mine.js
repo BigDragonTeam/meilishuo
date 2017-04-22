47800c9888f39cc52b9a8382103320ab462bab09
thl <852537973@qq.com>
2017-04-22 21:42:42 +0800

    'thl今日结束'

--- components/mine/mine.js
+++ components/mine/mine.js
@@ -9,4 +9,29 @@ angular.module('mineModule',[])
             })
     })
     .controller('mineCtrl',['$scope',function($scope){
+        $scope.changeColor1=function () {
+            $scope.isClassName1=true;
+            $scope.isClassName2=false;
+            $scope.isClassName3=false;
+            $scope.isClassName4=false;
+        };
+        $scope.changeColor2=function () {
+            $scope.isClassName1=false;
+            $scope.isClassName2=true;
+            $scope.isClassName3=false;
+            $scope.isClassName4=false;
+        };
+        $scope.changeColor3=function () {
+            $scope.isClassName1=false;
+            $scope.isClassName2=false;
+            $scope.isClassName3=true;
+            $scope.isClassName4=false;
+        };
+        $scope.changeColor4=function () {
+            $scope.isClassName1=false;
+            $scope.isClassName2=false;
+            $scope.isClassName3=false;
+            $scope.isClassName4=true;
+        };
+        $scope.changeColor1();
 }]);
