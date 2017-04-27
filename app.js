02de7f93633e1d077de197ecc9df21312b770c98
thl <852537973@qq.com>
2017-04-27 11:47:51 +0800

    'thl'

--- app.js
+++ app.js
@@ -1,50 +1,57 @@
-var app = angular.module('mls',["ui.router","me-lazyload",'angularCSS','homeModule','typeModule','cartModule','mineModule','homeSub1Module','homeSub2Module','homeSub3Module','detailModule'])
-    app.config(function($stateProvider, $urlRouterProvider){
-        $urlRouterProvider.otherwise('/home');
+var app = angular.module('mls', ["ui.router", "me-lazyload", 'angularCSS', 'homeModule', 'typeModule', 'cartModule', 'mineModule', 'homeSub1Module', 'homeSub2Module', 'homeSub3Module', 'detailModule'])
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+})
+    .factory('factory_getValue', function () {
+        var myData =[] ;
+
+        function _getter(i) {
+            return myData[i];
+        }
+
+        function _setter(a) {
+            myData = a
+        }
+
+        return {
+            getter: _getter,
+            setter: _setter
+        };
     })
-    .controller("tabCtrl",["$rootScope",function($rootScope){
-        $rootScope.changeColor1=function () {
-            $rootScope.isClassName1=true;
-            $rootScope.isClassName2=false;
-            $rootScope.isClassName3=false;
-            $rootScope.isClassName4=false;
+    .controller("tabCtrl", ["$rootScope",'factory_getValue', function ($rootScope,factory_getValue) {
+        $rootScope.changeColor1 = function () {
+            $rootScope.isClassName1 = true;
+            $rootScope.isClassName2 = false;
+            $rootScope.isClassName3 = false;
+            $rootScope.isClassName4 = false;
         };
-        $rootScope.changeColor2=function () {
-            $rootScope.isClassName1=false;
-            $rootScope.isClassName2=true;
-            $rootScope.isClassName3=false;
-            $rootScope.isClassName4=false;
+        $rootScope.changeColor2 = function () {
+            $rootScope.isClassName1 = false;
+            $rootScope.isClassName2 = true;
+            $rootScope.isClassName3 = false;
+            $rootScope.isClassName4 = false;
         };
-        $rootScope.changeColor3=function () {
-            $rootScope.isClassName1=false;
-            $rootScope.isClassName2=false;
-            $rootScope.isClassName3=true;
-            $rootScope.isClassName4=false;
+        $rootScope.changeColor3 = function () {
+            $rootScope.isClassName1 = false;
+            $rootScope.isClassName2 = false;
+            $rootScope.isClassName3 = true;
+            $rootScope.isClassName4 = false;
         };
-        $rootScope.changeColor4=function () {
-            $rootScope.isClassName1=false;
-            $rootScope.isClassName2=false;
-            $rootScope.isClassName3=false;
-            $rootScope.isClassName4=true;
+        $rootScope.changeColor4 = function () {
+            $rootScope.isClassName1 = false;
+            $rootScope.isClassName2 = false;
+            $rootScope.isClassName3 = false;
+            $rootScope.isClassName4 = true;
         };
         $rootScope.changeColor1();
 
-        $rootScope.toDetail=function (img1,title1,price1,sale1,cfav1) {
-            $rootScope.detailimg=img1;
-            $rootScope.detailtitle=title1;
-            $rootScope.detailprice=price1;
-            $rootScope.detailsale=sale1;
-            $rootScope.detailcfav=cfav1;
+        $rootScope.toDetail = function (img1, title1, price1, sale1, cfav1) {
+            factory_getValue.setter([img1,title1,price1,sale1,cfav1]);
         }
     }]);
-	 
 
 
-
-
-
-
-document.documentElement.style.fontSize = document.documentElement.clientWidth/7.5+ 'px';
-window.addEventListener('resize',function(){
-    document.documentElement.style.fontSize = innerWidth/7.5+'px';
+document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px';
+window.addEventListener('resize', function () {
+    document.documentElement.style.fontSize = innerWidth / 7.5 + 'px';
 });
--- detailPage/detailPage.js
+++ detailPage/detailPage.js
@@ -8,7 +8,13 @@ angular.module('detailModule',[])
                 css:'detailPage/detailPage.css'
             })
     })
-    .controller('detailCtrl',['$scope',function($scope){
+
+    .controller('detailCtrl',['$scope','factory_getValue',function($scope,factory_getValue){
+        $scope.detailimg= factory_getValue.getter(0);
+        $scope.detailtitle= factory_getValue.getter(1);
+        $scope.detailprice= factory_getValue.getter(2);
+        $scope.detailsale= factory_getValue.getter(3);
+        $scope.detailcfav= factory_getValue.getter(4);
 		$('.tab').empty();
         $scope.addToCar=function () {
             
